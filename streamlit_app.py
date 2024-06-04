66bdec8299667702578bab45f6e4bc01ff41a709
tomatyu <168719695+tomatyu@users.noreply.github.com>
2024-06-04 09:38:19 +0000

    ｄ

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,15 @@
 import streamlit as st
-import plotly.express as px
-pip install plotly
+import plotly.graph_objects as go
+
 # タイトルを表示
 st.title('World Map with Streamlit')
 
 # 地図の描画
-fig = px.scatter_geo(lat=[0], lon=[0])
+fig = go.Figure(data=go.Scattergeo(
+    lon = [0],
+    lat = [0],
+    mode = 'markers'
+))
+
 fig.update_geos(projection_type="natural earth")
 st.plotly_chart(fig)
