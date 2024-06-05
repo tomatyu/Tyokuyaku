f88598b3ee1a93fe325c2f6d7d6490525666a3d3
tomatyu <168719695+tomatyu@users.noreply.github.com>
2024-06-05 07:44:35 +0000

    ｆ

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-
+tittle("世界歴史検索装置")
 df = pd.DataFrame(
     np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
     columns=['lat', 'lon'])
