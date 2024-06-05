5059cd346076504fd0f13d79b8c0e60809f35549
tomatyu <168719695+tomatyu@users.noreply.github.com>
2024-06-05 07:51:36 +0000

    y

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-#df = pd.DataFrame(
+df = pd.DataFrame(
     np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
     columns=['lat', 'lon'])
 
