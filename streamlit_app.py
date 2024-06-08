4893a5038f1875bc7952f41a7f8843d65a88ea89
tomatyu <168719695+tomatyu@users.noreply.github.com>
2024-06-08 07:13:07 +0000

    d

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,15 @@
 import streamlit as st
 import pandas as pd
 
-# サイドバーに国名と首都を表示
-st.sidebar.write("国名: アメリカ合衆国")
-st.sidebar.write("首都: ワシントンDC")
+# フランスの緯度と経度
+france_latitude = 46.603354
+france_longitude = 1.888334
 
-# データフレームの作成
-usa_df = pd.DataFrame({
-    'lat': [38.8977],  # ワシントンDCの緯度
-    'lon': [-77.0365]  # ワシントンDCの経度
+# DataFrameを作成
+france_df = pd.DataFrame({
+    'lat': [france_latitude],
+    'lon': [france_longitude]
 })
 
-# メインエリアに地図を表示
-st.map(usa_df, zoom = 1)
+# 地図にピンを立てる
+st.map(france_df)
