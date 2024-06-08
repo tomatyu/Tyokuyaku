6ca335a4196e1e0ab13a703762f56e9a1942f3a4
tomatyu <168719695+tomatyu@users.noreply.github.com>
2024-06-08 06:53:01 +0000

    ｃ

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
 
-st.map()
+# フランスの緯度と経度
+france_latitude = 46.603354
+france_longitude = 1.888334
+
+# ピンを立てる
+st.map([[france_latitude, france_longitude]])
