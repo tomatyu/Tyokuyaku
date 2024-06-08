176703df1fe43c7be186bbe398757d96b9442baf
tomatyu <168719695+tomatyu@users.noreply.github.com>
2024-06-08 06:50:30 +0000

    ｓ

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-# 世界遺産の位置情報
-df = pd.DataFrame({'lat': [38.5342],'lot': [77.0212]})
+df = pd.DataFrame(
+    columns=['lat', 'lon'])
 
 st.map(df)
