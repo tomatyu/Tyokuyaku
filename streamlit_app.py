e539d667429d0103c37bea9f368023265d82e42e
tomatyu <168719695+tomatyu@users.noreply.github.com>
2024-06-08 06:38:45 +0000

    e

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 
 # 世界遺産の位置情報
-world_heritage_data = pd.DataFrame({
+a = pd.DataFrame({
     '名前': ['キリマンジャロ', 'エーゲ海の島々', 'クスコ歴史地区', 'タージ・マハル', 'グランドキャニオン国立公園', 'メキシコシティ歴史地区', 'クスコ歴史地区', 'モンテ・サン・ミシェル', 'シルクロード', 'イエローストーン国立公園'],
     'lat': [38.5342],
     'lot': [77.0212]
@@ -12,7 +13,7 @@ def main():
     st.title("世界遺産地図")
 
     # 地図上に世界遺産のピンを表示
-    st.map(world_heritage_data)
+    st.map(a)
 
 if __name__ == "__main__":
     main()
