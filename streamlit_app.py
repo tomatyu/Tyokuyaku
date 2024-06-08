84b5acd40709de2c6c63769abfe1c225de995751
tomatyu <168719695+tomatyu@users.noreply.github.com>
2024-06-08 06:53:58 +0000

    ｖ

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,15 @@
 import streamlit as st
+import pandas as pd
 
 # フランスの緯度と経度
 france_latitude = 46.603354
 france_longitude = 1.888334
 
-# ピンを立てる
-st.map([[france_latitude, france_longitude]])
+# DataFrameを作成
+france_df = pd.DataFrame({
+    'lat': [france_latitude],
+    'lon': [france_longitude]
+})
+
+# 地図にピンを立てる
+st.map(france_df)
