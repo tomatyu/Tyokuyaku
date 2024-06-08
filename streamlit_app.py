e8febb2ef15f409fd743b4d8402739fb63a2ec93
tomatyu <168719695+tomatyu@users.noreply.github.com>
2024-06-08 06:41:26 +0000

    d

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,10 +3,10 @@ import pandas as pd
 import numpy as np
 
 # 世界遺産の位置情報
-a = pd.DataFrame({'lat': [38.5342],'lot': [77.0212]})
+df = pd.DataFrame({'lat': [38.5342],'lot': [77.0212]})
 
 def main():
     st.title("世界遺産地図")
 
     # 地図上に世界遺産のピンを表示
-st.map(a)
+st.map(df)
