02dfd04a06206f80039e268a46cb002815a64b09
tomatyu <168719695+tomatyu@users.noreply.github.com>
2024-07-18 08:46:57 +0000

    e

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,5 +48,5 @@ st.title("テスト表示")
 
 if st.button("表示"):
     st.plotly_chart(fig, use_container_width=True)
-    st.plotly_chart(kig, use_container_width=True)
+    st.line_chart(kig, use_container_width=True)
   
