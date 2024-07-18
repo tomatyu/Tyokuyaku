915416955eceae0e85151416d1fd77daf2c72af9
tomatyu <168719695+tomatyu@users.noreply.github.com>
2024-07-18 07:49:38 +0000

    6

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,6 +19,6 @@ comparison_data = [major_countries]
 
 if st.button("点数を表示する"):
     comparison_data = [major_countries]
-    fig_comparison =(comparison_data, x='教科', y='点数', color='教科'),
+    fig_comparison =(comparison_data, x = '教科', y = '点数', color = '教科'),
     labels={'点数': '点数', '教科': '教科'}
     st.plotly_chart(fig_comparison)
