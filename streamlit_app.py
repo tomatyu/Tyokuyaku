ac1aeace18f0deeefd0c59d19bc4a4f5a6c9dda0
tomatyu <168719695+tomatyu@users.noreply.github.com>
2024-07-18 08:21:13 +0000

    u

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,11 +4,11 @@ import plotly.graph_objs as go
 import numpy as np
 import numbers
 
-a = st.text_number("")
-b = st.text_number("")
-c = st.text_number("")
-d = st.text_number("")
-e = st.text_number("")
+a = st.number_input("")
+b = st.number_input("")
+c = st.number_input("")
+d = st.number_input("")
+e = st.number_input("")
 
 kazu = [a,b,c,d,e]
 subject = ["国語","英語","数学","理科","社会"]
