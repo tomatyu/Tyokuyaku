0d4183bb8c22b1bff51123b9068ba9bec70a2a4d
tomatyu <168719695+tomatyu@users.noreply.github.com>
2024-07-19 07:09:13 +0000

    ｙ

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,23 +1,26 @@
 import streamlit as st
 import pandas as pd
 
-st.title('Excelデータのソート')
-
-# Excelファイルをアップロードするためのウィジェット
+# データを読み込む関数
 def load_data():
     return pd.read_excel("28.xlsx")
 
-if load_data is not None:
-    # pandasでExcelファイルを読み込む
-    df = pd.read_excel(load_data)
-    
-    # データを表示
+def main():
+    st.title('Excelデータのソート')
+
+    # データを読み込む
+    df = load_data()
+
+    # 元のデータを表示
     st.subheader('元のデータ')
     st.dataframe(df)
-    
+
     # 数値列で昇順にソートする
-    sorted_df = df.sort_values(by='緯度', ascending=True)
-    
+    sorted_df = df.sort_values(by='列名', ascending=True)
+
     # ソート後のデータを表示
     st.subheader('数値列を小さい順にソートした結果')
     st.dataframe(sorted_df)
+
+if __name__ == '__main__':
+    main()
