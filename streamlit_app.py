e3e4c82656374c3745e201144b66659aa28bec7f
tomatyu <168719695+tomatyu@users.noreply.github.com>
2024-07-23 07:00:17 +0000

    ０

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,10 +34,10 @@ def main():
     col1, col2 = st.columns(2)
     for label in button_labels:
         with col1:
-            if st.button(label):
+            if st.button(f"ボタン1: {label}"):
                 clicked_order.append(label)
         with col2:
-            if st.button(label):
+            if st.button(f"ボタン2: {label}"):
                 clicked_order.append(label)
 
     # 正しい順番を取得する
