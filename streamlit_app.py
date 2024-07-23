5804875c4359f3376f2ad12ec72c01958fd78bdf
tomatyu <168719695+tomatyu@users.noreply.github.com>
2024-07-23 07:25:21 +0000

    い

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,11 +6,14 @@ import random
 df = None
 
 # データをランダムに4行抽出して読み込む関数
-def load_data():
+
+if st.button("ランダムぅーっ‼"):
+  def load_data():
     global df
     df = pd.read_excel("28.xlsx")
     random_indices = random.sample(range(len(df)), min(4, len(df)))  # ランダムな行インデックスを取得する
     return df.iloc[random_indices]  # ランダムに抽出した行を取得する
+    
 
 def main():
     global df
@@ -21,16 +24,14 @@ def main():
     if df is None:
         df = load_data()
 
-    # 元のデータを表示
-    st.subheader('ランダムに選んだデータ（最大4行）')
-    st.dataframe(df)
+    
+    
 
     # 数値列で昇順にソートする
     sorted_df = df.sort_values(by='緯度', ascending=True)
 
     # ソート後のデータを表示
-    st.subheader('数値列を小さい順にソートした結果')
-    st.dataframe(sorted_df)
+    
 
     # データから4つのユニークな国名を取得する
     unique_countries = sorted_df['国名'].unique()
@@ -76,3 +77,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+    random
