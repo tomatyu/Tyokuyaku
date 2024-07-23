04635247a83a9a330e2b9e395f7f8fbbdda258da
tomatyu <168719695+tomatyu@users.noreply.github.com>
2024-07-23 08:01:37 +0000

    ８

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,57 +19,47 @@ def main():
     if df is None:
         load_data()
 
-    # 数値列で昇順にソートする
-    sorted_df = df.sort_values(by='緯度', ascending=True)
-
-    # ソート後のデータを表示
-    st.subheader('数値列を小さい順にソートした結果')
-    st.dataframe(sorted_df)
-
-    # データから4つのユニークな国名を取得する
-    unique_countries = sorted_df['国名'].unique()
     # ボタン用のラベルをランダムに選ぶ
+    sorted_df = df.sort_values(by='緯度', ascending=True)
+    unique_countries = sorted_df['国名'].unique()
     button_labels = random.sample(list(unique_countries), 4)
 
     # ボタンを2x2のグリッドに配置する
     col1, col2 = st.columns(2)
 
-    # ボタンの状態を保持するための辞書
-    button_state = {
+    # ボタンが押されたかどうかの状態を保持する辞書
+    button_clicked = {
         button_labels[0]: False,
         button_labels[1]: False,
         button_labels[2]: False,
         button_labels[3]: False
     }
 
-    # ボタンが押された場合、そのボタンの状態を反転する
+    # ボタンが押された場合、そのボタンの状態を更新する
     if st.button(button_labels[0]):
-        button_state[button_labels[0]] = not button_state[button_labels[0]]
+        button_clicked[button_labels[0]] = True
     if st.button(button_labels[1]):
-        button_state[button_labels[1]] = not button_state[button_labels[1]]
+        button_clicked[button_labels[1]] = True
     if st.button(button_labels[2]):
-        button_state[button_labels[2]] = not button_state[button_labels[2]]
+        button_clicked[button_labels[2]] = True
     if st.button(button_labels[3]):
-        button_state[button_labels[3]] = not button_state[button_labels[3]]
+        button_clicked[button_labels[3]] = True
 
     # ボタンの状態に応じてメッセージを表示する
     with col1:
-        if button_state[button_labels[0]]:
+        if button_clicked[button_labels[0]]:
             st.write(f'クリックされたボタン: {button_labels[0]}')
     with col2:
-        if button_state[button_labels[1]]:
+        if button_clicked[button_labels[1]]:
             st.write(f'クリックされたボタン: {button_labels[1]}')
 
     with col1:
-        if button_state[button_labels[2]]:
+        if button_clicked[button_labels[2]]:
             st.write(f'クリックされたボタン: {button_labels[2]}')
     with col2:
-        if button_state[button_labels[3]]:
+        if button_clicked[button_labels[3]]:
             st.write(f'クリックされたボタン: {button_labels[3]}')
 
-    # 表示されたデータを消去する
-    st.subheader('')
-    st.dataframe(None)
 
 if __name__ == '__main__':
     main()
