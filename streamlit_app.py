3336627f20390d003381aa50d081af342c3dbb30
tomatyu <168719695+tomatyu@users.noreply.github.com>
2024-07-26 09:24:16 +0000

    ｘ

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,37 +1,18 @@
 import streamlit as st
-import pandas as pd
 
-# Excelをロードする関数
-def load_data():
-    try:
-        return pd.read_excel("1s.xlsx")  # 拡張子を確認してください
-    except Exception as e:
-        st.error(f"ファイルの読み込みに失敗しました: {e}")
-        return pd.DataFrame()  # 空のデータフレームを返す
+@st.dialog("Cast your vote")
+def vote(item):
+    st.write(f"Why is {item} your favorite?")
+    reason = st.text_input("Because...")
+    if st.button("Submit"):
+        st.session_state.vote = {"item": item, "reason": reason}
+        st.rerun()
 
-# データの読み込み
-countries_df = load_data()
-
-# タイトル
-st.title("$古文直訳writer$")
-st.write("上から文節ごとに入力していってください。（最大10単語適応）")
-
-# 単語入力欄
-inputs = [st.text_input(f"単語 {i+1}") for i in range(10)]
-
-# 「国を表示」ボタンがクリックされたときの処理
-if st.button('直訳を表示'):
-    meanings = []
-    for word in inputs:
-        if word.strip() != "":  # 空でない単語のみ検索
-            kv = countries_df[countries_df["古文"] == word]
-            if not kv.empty:
-                meanings.append(kv["意味"].iloc[0])
-            else:
-                meanings.append(f"'{word}' の検索結果が見つかりませんでした")
-        else:
-            meanings.append("")
-    
-    # 検索結果の表示
-    st.write("検索結果:")
-    st.write(" ".join(meanings))
+if "vote" not in st.session_state:
+    st.write("Vote for your favorite")
+    if st.button("A"):
+        vote("A")
+    if st.button("B"):
+        vote("B")
+else:
+    f"You voted for {st.session_state.vote['item']} because {st.session_state.vote['reason']}"
