ed18662c09f07b1bd63d0201d1380cbd3d2dffd5
tomatyu <168719695+tomatyu@users.noreply.github.com>
2024-08-26 13:59:42 +0000

    ｄ

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,27 +2,34 @@ import streamlit as st
 import pydeck as pdk
 
 # Streamlitのタイトル
-st.title('3D球体地球儀の表示')
+st.title('地球儀風のデータ表示')
 
-# pydeckの地球儀を作成
+# pydeckの地球儀風のビジュアライゼーションを作成
 deck = pdk.Deck(
     initial_view_state=pdk.ViewState(
         latitude=0,  # 緯度
         longitude=0,  # 経度
         zoom=1,  # ズームレベル
-        pitch=0  # 傾き
+        pitch=50  # 傾き
     ),
     layers=[
         pdk.Layer(
-            'GlobeLayer',
-            data=[{}],  # データは空でも球体の表示には影響なし
-            get_position='[longitude, latitude]',
-            radius=1,
-            extruded=True
+            'ArcLayer',
+            data=[
+                {
+                    'start': [-122.4194, 37.7749],  # サンフランシスコの座標
+                    'end': [139.6917, 35.6895],  # 東京の座標
+                }
+            ],
+            get_source_position='start',
+            get_target_position='end',
+            get_source_color=[255, 0, 0],
+            get_target_color=[0, 0, 255],
+            radius=1000,
+            width_scale=10
         )
-    ],
-    views=[pdk.view.GlobeView()]  # GlobeViewを使って球体表示
+    ]
 )
 
-# Streamlitで地球儀を表示
+# Streamlitで地球儀風のビジュアライゼーションを表示
 st.pydeck_chart(deck)
