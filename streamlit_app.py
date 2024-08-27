a1902b92a6da1f29085859764fe9e23057a4e791
tomatyu <168719695+tomatyu@users.noreply.github.com>
2024-08-27 04:41:53 +0000

    r

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,6 +29,7 @@ if st.button('直訳を表示'):
             kv = countries_df[countries_df["古文"] == word]
             if not kv.empty:
                 meanings.append(kv["意味"].iloc[0])
+                meanings.append("/")
             else:
                 meanings.append(f"{word}")
         else:
