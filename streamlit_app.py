6dbaff1d9a4fd07afd60d932917d66fc35669e73
tomatyu <168719695+tomatyu@users.noreply.github.com>
2024-08-28 16:39:14 +0000

    ｄ

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,55 +11,30 @@ def load_data():
         return pd.DataFrame()  # 空のデータフレームを返す
 
 # 初期設定
-st.title('古文ツール')
+st.title('古文自動直訳ツール')
 
-# サイドバーで機能の選択
-option = st.sidebar.selectbox('選択してください', ['文節分割ツール', '直訳検索ツール'])
+# 入力ボックス
+text_input = st.text_area('古文テキストを入力してください', height=200)
 
-# 文節分割ツール
-if option == '文節分割ツール':
-    st.header('古文の文節分割ツール')
-    
-    # 入力ボックス
-    text_input = st.text_area('古文テキストを入力してください', height=200)
-    
-    # 文節分割処理
-    if text_input:
-        tokenizer = Tokenizer()
-        tokens = tokenizer.tokenize(text_input)
-        
-        # 文節ごとに分割
-        segments = [token.surface for token in tokens]
-        
-        # 結果の表示
-        st.subheader('文節ごとに分割した結果')
-        st.write('/ '.join(segments))
+# データの読み込み
+countries_df = load_data()
 
-# 直訳検索ツール
-elif option == '直訳検索ツール':
-    st.header('古文直訳ツール')
+# 文節分割処理
+if text_input:
+    tokenizer = Tokenizer()
+    tokens = tokenizer.tokenize(text_input)
     
-    # データの読み込み
-    countries_df = load_data()
+    # 文節ごとに分割
+    segments = [token.surface for token in tokens]
+
+    # 直訳の検索と結果の表示
+    meanings = []
+    for segment in segments:
+        kv = countries_df[countries_df["古文"] == segment]
+        if not kv.empty:
+            meanings.append(kv["意味"].iloc[0])
+        else:
+            meanings.append(f"'{segment}' の検索結果が見つかりませんでした")
     
-    # サイドバーでの単語入力
-    st.sidebar.header("単語入力")
-    inputs = [st.sidebar.text_input(f"単語 {i+1}", key=f"input_{i}") for i in range(60)]
-    
-    st.write("上から文節ごとに:red[ひらがなで]入力してください。（最大60単語適応）")
-    
-    # 「直訳を表示」ボタンがクリックされたときの処理
-    if st.button('直訳を表示'):
-        meanings = []
-        for word in inputs:
-            if word.strip() != "":  # 空でない単語のみ検索
-                kv = countries_df[countries_df["古文"] == word]
-                if not kv.empty:
-                    meanings.append(kv["意味"].iloc[0])
-                else:
-                    meanings.append(f"'{word}' の検索結果が見つかりませんでした")
-            else:
-                meanings.append("")
-        
-        # 検索結果を表示
-        st.write(" ".join(meanings))
+    st.subheader('文節ごとの直訳結果')
+    st.write(" / ".join(meanings))
